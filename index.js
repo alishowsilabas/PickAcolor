1434c4443929fa423ea0ef216f54df54d7880211
alishowsilabas <alissonsilabas@gmail.com>
2023-09-25 10:31:38 -0300

    semi completo

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const colors = ['Red', 'Green', 'Blue', 'Yellow', 'Purple'];
 const button = document.getElementById('btn');
+const color = document.querySelector('.color');
 
 button.addEventListener('click', function() {
-    this.style.background = colors[Math.floor(Math.random() * colors.length)]
+    const randomColor = genRandomColor()
+    this.style.background = colors[randomColor]
+    color.textContent = colors[randomColor]
 })
+
+function genRandomColor() {
+    return Math.floor(Math.random() * colors.length)
+}
